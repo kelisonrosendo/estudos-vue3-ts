828ae63a45e08eecb1d7d0c89a5221a444d67434
Kelison Rosendo da Rocha <kelison.rocha@zitrus.com.br>
2023-10-18 17:31:36 -0300

    feature: adicionando contador de personagens no componente NavigationMenu

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="flex items-center">
-    <ul class="w-full h-full flex items-center gap-10">
+  <nav class="w-full flex items-center justify-between">
+    <ul class="flex gap-10">
       <li class="flex items-center gap-2">
         <Home :size="22" />
         <RouterLink to="/"> Home </RouterLink>
@@ -10,9 +10,22 @@
         <RouterLink to="/characters"> Characters </RouterLink>
       </li>
     </ul>
+    <Badge v-if="charactersRoute" class="bg-green-300 text-slate-800 text-sm">
+      Characters found: {{ charactersCounter }}
+    </Badge>
   </nav>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { useCharacterStore } from '@/stores/character'
 import { Home, Contact2 } from 'lucide-vue-next'
+import { Badge } from '@/components/ui/badge'
+import { computed } from 'vue'
+
+const route = useRoute()
+const charactersRoute = computed(() => route.name === 'characters')
+
+const store = useCharacterStore()
+const charactersCounter = computed(() => store.info.count || 0)
 </script>
